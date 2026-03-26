8cdfe1a81c2f487c9b1c60d13fc265dc61cba864
thesignumt <tcrrxzz@gmail.com>
2026-03-26 20:53:05 +0000

    style: for some reason my lsp decided to change indent

--- include/token.h
+++ include/token.h
@@ -5,14 +5,14 @@
 #include <string.h>
 
 typedef enum {
-  STDIN_REDIRECT,  // <
-  STDOUT_REDIRECT, // >
-  REDIRECT_APPEND, // >>
-  PIPE,            // |
-  BACKGROUND,      // &
-  IDENTIFIER,      // commands & arguments
-  STRING,          // "quoted args"
-  END_OF_FILE,     // end of input
+  STDIN_REDIRECT,   // <
+  STDOUT_REDIRECT,  // >
+  REDIRECT_APPEND,  // >>
+  PIPE,             // |
+  BACKGROUND,       // &
+  IDENTIFIER,       // commands & arguments
+  STRING,           // "quoted args"
+  END_OF_FILE,      // end of input
   UNKNOWN
 } TokenType;
 
