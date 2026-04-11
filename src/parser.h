79e4370bad4bc5e567bb3c2d5d70ab796236f885
thesignumt <tcrrxzz@gmail.com>
2026-04-11 15:44:10 +0100

    not needed yet

--- src/parser.h
+++ src/parser.h
@@ -31,35 +31,4 @@ void free_pipeline(Pipeline *pipeline);
 
 Pipeline *Parse(Tokens *tokens);
 
-typedef enum {
-  ARG_PATH,     // filesystem path
-  ARG_LITERAL,  // plain arguments / words
-  ARG_OPTION,   // e.g. -v, --help
-  ARG_UNKNOWN   // anything else
-} ArgType;
-
-static inline ArgType validate_arg(const char *lexeme) {
-  if (!lexeme || is_eof_char(lexeme[0])) return ARG_UNKNOWN;
-
-  // Check if it's a path (contains / or starts with . or ~)
-  if (strchr(lexeme, '/') || lexeme[0] == '.' || lexeme[0] == '~') {
-    return ARG_PATH;
-  }
-
-  // Check if it's an option (starts with -)
-  if (lexeme[0] == '-') return ARG_OPTION;
-
-  // Check if it's a literal (alphanumeric or _)
-  if (isalpha(lexeme[0]) || lexeme[0] == '_') {
-    for (size_t i = 1; i < strlen(lexeme); i++) {
-      if (!isalnum(lexeme[i]) && lexeme[i] != '_') {
-        return ARG_UNKNOWN;
-      }
-    }
-    return ARG_LITERAL;
-  }
-
-  return ARG_UNKNOWN;
-}
-
 #endif  // PARSER_H_
